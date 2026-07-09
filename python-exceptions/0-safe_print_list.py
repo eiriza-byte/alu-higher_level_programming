6885999fc2128160edda04ee5b6957937fef5677
eiriza-byte <e.iriza@alustudent.com>
2026-07-09 14:22:52 -0700

    Add script that prints x elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that defines safe_print_list."""
+
+
+def safe_print_list(my_list=[], x=0):
+    """Print x elements of a list and return the number printed."""
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
