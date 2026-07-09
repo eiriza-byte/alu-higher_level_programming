9265b9339d4ff09a44c994b005c034f547c8b612
eiriza-byte <e.iriza@alustudent.com>
2026-07-09 12:12:16 -0700

    Add script that deletes the item at a specific position in a list.

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    """Delete the item at a specific position in a list."""
+    """Delete the item at a specific position in a list, in place."""
     if idx < 0 or idx >= len(my_list):
         return my_list
-    return my_list[:idx] + my_list[idx + 1:]
+    del my_list[idx]
+    return my_list
